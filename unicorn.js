b8d3921bc401f719a7088a0ab9066a11d8492fb7
Adrian <71754820+lona9a@users.noreply.github.com>
2021-07-17 13:49:48 +0200

    Update unicorn.js

--- unicorn.js
+++ unicorn.js
@@ -1,9 +1,4 @@
 // Daniel Shiffman
-// https://thecodingtrain.com/CodingChallenges/147-chrome-dinosaur.html
-// https://youtu.be/l0HoJHc-63Q
-
-// Google Chrome Dinosaur Game (Unicorn, run!)
-// https://editor.p5js.org/codingtrain/sketches/v3thq2uhk
 
 class Unicorn {
   constructor() {
